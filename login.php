d3e2982b4c324f7aa8902388ea8b20c9c4a2e5a2
Giulio Montagner <me@giuliomontagner.com>
2015-04-03 16:41:37 +0200

    minor fixes - Added favicon to login page - Fixed deprecated php tag in configuration file

--- login.php
+++ login.php
@@ -41,6 +41,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title><?=DISPLAY_TITLE?></title>
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link rel="icon" href="favicon.ico" type="image/x-icon">
 <link rel="stylesheet" href="css/login_style.css" type="text/css" />
 </head>
 
